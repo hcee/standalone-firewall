326376feb5f4430579bbcd8deadd1b7690844cfd
Data Comm <dc@milliways.bcit.ca>
2020-01-18 12:09:18 -0800

    allow dns and dhcp

--- firewall.sh
+++ firewall.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 DEFAULT_CHAINS=("INPUT" "OUTPUT" "FORWARD")
-LAN_IFACE="enp2s0"
+LAN_IFACE="eno1"
 VALID_TCP_PORTS=("22")
 
 #---------shortcut to resetting the default policy---------
@@ -28,18 +28,22 @@ for CHAIN in "${DEFAULT_CHAINS[@]}"; do
 done
 
 #----user-defined chains-------
-iptables -N ssh
-iptables -N www
 
-# #------------allow DNS and DHCP traffic-------------------
-# iptables -A OUTPUT -p udp -o eth0 --dport 53 -j ACCEPT
-# iptables -A INPUT -p udp -i eth0 --sport 53 -j ACCEPT
-# iptables -I INPUT -i "$LAN_IFACE" -p udp --dport 67:68 --sport 67:68 -j ACCEPT
+
+#------------allow DNS and DHCP traffic-------------------
+iptables -A OUTPUT -p udp --dport 53 -j ACCEPT
+iptables -A INPUT -p udp --sport 53 -j ACCEPT
+iptables -A OUTPUT -p tcp --dport 53 -j ACCEPT
+iptables -A INPUT -p tcp --sport 53 -j ACCEPT
+iptables -I INPUT -p udp --dport 67:68 --sport 67:68 -j ACCEPT
+
+iptables -A INPUT -i lo -j ACCEPT
+iptables -A OUTPUT -o lo -j ACCEPT
 
 #----------ACCEPT packets on ssh and www----------------------
 #inbound/outbound TCP packets on allowed ports
-iptables -A INPUT -p TCP -m multiport --sport "$VALID_TCP_PORTS" -m state --state NEW,ESTABLISHED -j ACCEPT
-iptables -A INPUT -p TCP -m multiport --dport "$VALID_TCP_PORTS" -m state --state NEW,ESTABLISHED -j ACCEPT
+iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+
 
 # #inbound/outbound UDP packets on allowed ports
 # iptables -A FORWARD -p UDP -m multiport --sport "$VALID_UDP_PORTS" -j ACCEPT
@@ -50,3 +54,5 @@ iptables -A INPUT -p TCP -m multiport --dport "$VALID_TCP_PORTS" -m state --stat
 # Permit inbound/outbound www packets
 # Drop inbound traffic to port 80 (http) from source ports less than 1024
 # Drop all incoming packets from reserved port 0 as well as outbound traffic to port 0
+
+iptables -L -n
