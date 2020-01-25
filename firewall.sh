585bbc5a5431f868944a625aaef37bf03d51b426
Data Comm <dc@milliways.bcit.ca>
2020-01-25 00:16:31 -0800

    final firewall

--- firewall.sh
+++ firewall.sh
@@ -14,14 +14,6 @@ if [ "$1" = "flush" ]; then
   echo -e "-------------------------------------------\n"
   iptables -L -n
   exit 0
-# elif [ "$1" = "reset"]; then
-#   iptables -L -n -Z -v
-#   exit 0
-# elif [ "$1" = "show"]; then
-#   iptables -L -v -n -x
-#   exit 0
-# else
-#   exit 0
 fi
 
 #-----------Set the default policies to DROP--------------
@@ -40,15 +32,15 @@ iptables -N ingress
 iptables -N egress
 
 # Drop all incoming packets from reserved port 0 as well as outbound traffic to port 0
-iptables -A INPUT -j DROP -p tcp --sport 0
-iptables -A INPUT -j DROP -p udp --sport 0
-iptables -A INPUT -j DROP -p tcp --dport 0
-iptables -A INPUT -j DROP -p udp --dport 0
+iptables -A INPUT -p tcp --sport 0 -j DROP
+iptables -A INPUT -p udp --sport 0 -j DROP
+iptables -A INPUT -p tcp --dport 0 -j DROP
+iptables -A INPUT -p udp --dport 0 -j DROP
 
-iptables -A OUTPUT -j DROP -p tcp --sport 0
-iptables -A OUTPUT -j DROP -p udp --sport 0
-iptables -A OUTPUT -j DROP -p tcp --dport 0
-iptables -A OUTPUT -j DROP -p udp --dport 0
+iptables -A OUTPUT -p tcp --sport 0 -j DROP
+iptables -A OUTPUT -p udp --sport 0 -j DROP
+iptables -A OUTPUT -p tcp --dport 0 -j DROP
+iptables -A OUTPUT -p udp --dport 0 -j DROP
 
 #------------allow DNS and DHCP traffic-------------------
 iptables -A OUTPUT -p udp -m udp --dport 53 -j ACCEPT
@@ -59,25 +51,25 @@ iptables -A INPUT -p tcp -j ingress
 iptables -A OUTPUT -p tcp -j egress
 
 #------------------Permit inbound SSH-------------
-iptables -A ingress -p tcp -s 192.168.0.0/24 --dport 22 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT #working now!!!!!
-iptables -A egress -p tcp -d 192.168.0.0/24 --sport 22 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT #--this is for sure correct
+iptables -A ingress -p tcp -s 192.168.0.0/24 --dport 22 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
+iptables -A egress -p tcp -d 192.168.0.0/24 --sport 22 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 
 # outbound ssh packets
-iptables -A ingress -p tcp -s 192.168.0.0/24 --sport 22 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT #working now!!!!!
-iptables -A egress -p tcp -d 192.168.0.0/24 --dport 22 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT #--this is for sure correct
+iptables -A ingress -p tcp -s 192.168.0.0/24 --sport 22 -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+iptables -A egress -p tcp -d 192.168.0.0/24 --dport 22 -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
 
 # Drop inbound traffic to port 80 (http) from source ports less than 1024
 iptables -A ingress -p tcp -m tcp --sport 0:1023 --dport 80 -j DROP
 
 #----------Permit inbounds WWW---------------------
-iptables -A ingress -p tcp -m multiport --dport "$VALID_TCP_PORTS" -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT #working now!!!!!
-iptables -A egress -p tcp -m multiport --sport "$VALID_TCP_PORTS" -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT #---this works also.
+iptables -A ingress -p tcp -m multiport --dport "$VALID_TCP_PORTS" -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT 
+iptables -A egress -p tcp -m multiport --sport "$VALID_TCP_PORTS" -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
 
 # outbound www packet
-iptables -A ingress -p tcp -m multiport --sport "$VALID_TCP_PORTS" -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT #working now!!!!!
-iptables -A egress -p tcp -m multiport --dport "$VALID_TCP_PORTS" -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT #---this works also.
+iptables -A ingress -p tcp -m multiport --sport "$VALID_TCP_PORTS" -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT 
+iptables -A egress -p tcp -m multiport --dport "$VALID_TCP_PORTS" -m conntrack --ctstate NEW,ESTABLISHED -j ACCEPT
 
 # Drop all inbound SYN packets
-iptables -A INPUT -p tcp -m tcp --syn -j DROP
+iptables -A INPUT -p tcp --syn -j DROP
 
 iptables -L -n
